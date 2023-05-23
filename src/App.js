60f32d407ea32fdb17b29db877beabfc367c8f2d
anandhu-as <anandhuas010203@gmail.com>
2023-05-23 11:12:12 +0530

    Links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "../src/components/styles/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
@@ -7,6 +7,9 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/About" Component={About} />
